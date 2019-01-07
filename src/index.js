550f63f078d6e2f2281d6526d9981a3cbc1b9ce3
alex <alex@ritzycapital.com>
2019-01-06 23:15:55 -0600

    configuration server

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require('path');
+
+//initilizations
+const app = express();
+
+//settings
+app.set('port' , process.env.PORT || 3000);
+app.set('views' , path.join(__dirname , 'views'))
+app.engine('.hbs' , exphbs({
+    defaultLayout: 'main' ,
+    layoutsDir: path.join(app.get('views') , 'layouts'),
+    partialsDir: path.join(app.get('views') , 'partials'),
+    extname: '.hbs',
+    handlebars: require('./lib/handlebars')
+}));
+app.set('view engine' , '.hbs')
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({'extended' : false}));
+app.use(express.json());
+
+//global varibles
+app.use((req , res , next) => {
+    
+    next();
+});
+
+//routes
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links' , require('./routes/links'));
+
+//public 
+app.use(express.static(path.join(__dirname , 'public')))
+
+//starting the server
+app.listen(app.get('port') , () => {
+    console.log(`server on port ${app.get('port')}`);
+});
